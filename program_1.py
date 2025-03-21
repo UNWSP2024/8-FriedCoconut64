e5fa0de3b3bd8cc35fc84e8db88e73d93f8371a8
FriedCoconut64 <vansterapples@gmail.com>
2025-03-21 14:25:32 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,19 +1,16 @@
-# Program #1: Initials
-# Write a program that gets a string containing a person's first, middle, and last names, 
-# and displays their first, middle, and last initials.  
-# For example, if the user enters John William Smith, the program should display J. W. S.
-
-# Add your logic starting on line 11
-
 def initials_generator(personsName):
 
-    personsInitials = ""
-    #    Add your logic here
 
+    personsInitials = ""
+    seperate_names = personsName.split()
+    personsInitials = ".".join([names[0] for names in seperate_names]) + '.'
+    
     return personsInitials.strip()
 
-personsName = input('Enter the users first, middle, and last name')
+personsName = input('Enter the users first, middle, and last name:')
 
 initials = initials_generator(personsName)
 
 print(initials)
+
+# Program #1, Donovan Thompson 3/21/2025

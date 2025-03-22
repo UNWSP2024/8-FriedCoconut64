8a9a0a3b8ee2a93fa641ba47ecf3bb5100b8231e
FriedCoconut64 <vansterapples@gmail.com>
2025-03-21 21:33:11 -0500

    Update program_5.py

--- program_5.py
+++ program_5.py
@@ -1,5 +1,21 @@
-# Program #5: Course Info
-# Write a program that has the user input a bunch of course ID and course name pairs.  
-# For example a course ID could be "COS 2005" and the course name could be "Python Programming."   
-# Then ask the user for a subject (like "COS"). 
-# Finally, the program will display the ID and name of all the courses having that subject.
+def course_info(course_index):
+    subject = str(input("Select a subject (eg. COS):")).upper()
+    result = [course_name for course_id, course_name in course_index.items() if course_id.startswith(subject)]
+    return result if result else "Error: Subject not found"
+
+def main():
+    course_index = {}
+    while True:
+        course_id = input("Please enter a course ID (eg. COS 2005):").upper()
+        course_name = input("Please enter the name of that course (eg. Python Programming):")
+        course_index[course_id] = course_name
+        i = input("Would you like to enter another course? (type Y for yes or anything else for no):")
+        if i.lower() != 'y':
+            break
+
+    subject_list = course_info(course_index)
+
+    print("Here are a list of courses in that subject:", subject_list)
+
+main()
+# Program #5, Donovan Thompson 3/21/2025
